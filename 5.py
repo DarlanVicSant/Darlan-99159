cbedf82ba20e1cd6ccf6f1abdb1a9263b1e8d5bc
DarlanVicSant <darlan.v.santos@ba.estudante.senai.br>
2025-09-11 16:54:01 -0300

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,18 +1,19 @@
-# Escrever um algoritimo que mostra os 
-# numero pares entre 100 e 120
-
 import streamlit as st
 import time
 
-st.title("Atividade 4")
+st.title("Atividade 6")
 
-st.write("Escreva um numero e multiplique ele por 2")
+st.header("Contagem regressiva")
 
-numero = st.number_input("Digite um número: ", step=1)
+st.write("Solicite a um usuario um número e faça a contagem regressiva")
 
+numero = st.number_input("Digite um numero ", step = 1, min_value = 0)
 
 if st.button("Iniciar"):
-    for i in range(1,11):
-        st.info(f"{numero} x {i} = {numero * i}")
-        time.sleep(1)
+    for i in range(numero,0, -1):
+        st.warning(i)
+        time.sleep(i)
     st.header("Fim")
+    # st.balloons()
+else:
+    st.info("Informe um número")    
--- atr.py
+++ atr.py
@@ -1,18 +1,29 @@
 import streamlit as st
 import time
 
-st.title("Laço repitiçao")
- 
-st.header("Contagem numeros pares")
+st.title("Atividade 7")
 
-pares = (num for num in range(100,121) if num & 2 == 0)
+st.header("Numeros inteiros")
 
-if st.button("Iniciar"):
- st.write("Os numeros pares são:") 
- for p in pares: 
-    st.write(p)
-
-    
+st.write("Solicite a um usuario 5 número inteiro e some")
 
 
-    
+st.title("Calculadora de Soma de 5 Números")
+
+st.write("Insira 5 números inteiros para calcular a soma.")
+
+
+num1 = st.number_input("Número 1", value=0, step=1)
+num2 = st.number_input("Número 2", value=0, step=1)
+num3 = st.number_input("Número 3", value=0, step=1)
+num4 = st.number_input("Número 4", value=0, step=1)
+num5 = st.number_input("Número 5", value=0, step=1)
+
+
+soma = num1 + num2 + num3 + num4 + num5
+
+if st.button("Iniciar soma"):
+    st.subheader("Resultado da Soma")
+    st.write(f"A soma dos números é: **{soma}**")
+else:
+    st.info("Informe os numeros")
